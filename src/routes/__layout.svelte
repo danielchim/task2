05eb2e1297c1b865f71e0de68a7fe3559b5c56bb
danielchim <kahungchim@gmail.com>
2022-05-29 20:14:38 +0800

    inital commbit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,38 @@
+<script>
+	import {
+		Header,
+		SideNav,
+		SideNavItems,
+		SideNavLink,
+		SideNavDivider,
+		SkipToContent,
+		Content,
+		TextInput,
+
+	} from "carbon-components-svelte";
+
+	let isSideNavOpen = false;
+</script>
+
+<Header company="Stonks" platformName="Carbon Svelte" bind:isSideNavOpen>
+	<svelte:fragment slot="skip-to-content">
+		<SkipToContent />
+	</svelte:fragment>
+</Header>
+
+<SideNav bind:isOpen={isSideNavOpen}>
+	<SideNavItems>
+		<SideNavLink text="Home" />
+		<SideNavDivider />
+		<TextInput placeholder="Enter user name..." />
+		<SideNavLink text="Link 1" />
+		<SideNavLink text="Link 2" />
+		<SideNavLink text="Link 3" />
+	</SideNavItems>
+</SideNav>
+
+<Content>
+	<main>
+		<slot />
+	</main>
+</Content>
---
+++ src/routes/index.svelte
@@ -0,0 +1,52 @@
+<script>
+	import {
+		Content,
+		Grid,
+		Row,
+		Column,
+		Button,
+		Modal,
+		TextInput,
+	} from "carbon-components-svelte";
+	import "carbon-components-svelte/css/white.css";
+	import Hidden from '../lib/modal/Hindden.svelte'
+	let open = false;
+	let show;
+	let shown;
+</script>
+
+<Content>
+	<Grid fullWidth padding>
+		<Row>
+			<h1>All stocks</h1>
+			<Button on:click={() => (open = true)}>New stock</Button>
+
+			<Modal
+				bind:open
+				modalHeading="New stock"
+				primaryButtonText="Confirm"
+				secondaryButtonText="Cancel"
+				on:click:button--secondary={() => (open = false)}
+				on:open
+				on:close
+				on:submit
+			>
+				<Grid padding>
+					<Column>
+						<TextInput labelText="Enter a stock to track" />
+						<Button on:click={show}>Show advanced options</Button>
+						<Hidden bind:shown bind:show>
+							<h4>Hidden content</h4>
+							<TextInput labelText="Enter a stock to track" />
+							<TextInput labelText="Enter a stock to track" />
+							<TextInput labelText="Enter a stock to track" />
+							<TextInput labelText="Enter a stock to track" />
+							<TextInput labelText="Enter a stock to track" />
+							<TextInput labelText="Enter a stock to track" />
+						</Hidden>
+					</Column>
+				</Grid>
+			</Modal>
+		</Row>
+	</Grid>
+</Content>
